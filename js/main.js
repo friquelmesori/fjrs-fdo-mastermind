5592e6de4630f3ffcc198f52722b239d7a78c265
friquelmesori <friquelmesori@gmail.com>
2023-06-25 23:03:46 +0200

    SessionStorage que recoge el input/nombre del jugador

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 // SessionStorage USUARIO
 
-// const saveJugador = () => {
+const saveJugador = () => {
 
-//     let jugador = document.getElementById("nombreJugador").value;
+    let jugador = document.getElementById("nombreJugador").value;
 
-//     sessionStorage.setItem("usuario", jugador);
-//     window.location.href = "../pages/prueba.html"
-// }
+    sessionStorage.setItem("usuario", jugador);
+
+    guardar.addEventListener ('click', e => {
+        sessionStorage.setItem('usuario', input.value)
+        window.location.href = "../pages/prueba.html"
+    }); 
+}
