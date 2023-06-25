849a5db0a97b3277358fc2551cd4c1b7d5487cbf
friquelmesori <friquelmesori@gmail.com>
2023-06-25 21:22:13 +0200

    Creada la parte visual de las paginas tablero añadido check_icon

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// SessionStorage USUARIO
+
+// const saveJugador = () => {
+
+//     let jugador = document.getElementById("nombreJugador").value;
+
+//     sessionStorage.setItem("usuario", jugador);
+//     window.location.href = "../pages/prueba.html"
+// }
